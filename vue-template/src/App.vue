4d559c1b677ced29cc93e3d52a7f920e978b10a1
Livia Belizario <liviabelirocha@outlook.com>
2021-10-19 10:02:57 -0300

    Adicionando desafio

---
+++ vue-template/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Home
+      msg="Seja bem vindo(a), certifique-se de ler as instruções na página do github do desafio."
+    />
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style>
+#app {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font: 400 16px Roboto, sans-serif;
+}
+</style>
---
+++ vue-template/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+#home {
+  max-width: 30rem;
+}
+</style>
